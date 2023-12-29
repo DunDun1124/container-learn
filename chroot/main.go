bcfc9a5a1ce14bdc6716513fd46a55437bf91819
jq <jiangqiang@hjfruit.com>
2023-12-29 14:17:44 +0800

    feat: 基本 chroot 的使用

--- chroot/main.go
+++ chroot/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
 	"syscall"
 )
+
+const ShellToUse = "/bin/bash"
+
+func shellOut(command string) (string, string, error) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(ShellToUse, "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return stdout.String(), stderr.String(), err
+}
 
 // 注意,使用goland时，需要在 setting -> go -> build tag 切换 os 为 linux
 func main() {
@@ -20,28 +33,28 @@ func main() {
 		fmt.Printf("fail to chroot %v\n", err)
 	}
 
-	cmd := exec.Command("/bin/ls", "-l")
-	_, err = cmd.Output()
+	out, s, err := shellOut("ls -l")
 	if err != nil {
 		fmt.Println("Failed to execute command:", err)
-		//return
+	} else {
+		fmt.Println("result is:", out+s)
 	}
 
 	err = oldRootF.Chdir()
 	if err != nil {
 		fmt.Printf("chdir() err: %v", err)
 	}
-	err = syscall.Chroot("/")
+	err = syscall.Chroot(".")
 	if err != nil {
 		fmt.Printf("chroot back err: %v", err)
 	}
 
-	cmd = exec.Command("/bin/ls", "-l")
-	output, err := cmd.Output()
+	out, s, err = shellOut("ls -l")
 	if err != nil {
 		fmt.Println("Failed to execute command:", err)
 		return
+	} else {
+		fmt.Println("result is:", out+s)
 	}
 
-	fmt.Println(string(output))
 }
