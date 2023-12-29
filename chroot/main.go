14235dc33c36d5ae7fcd8c8985dadff6512edb6c
jq <jiangqiang@hjfruit.com>
2023-12-29 14:00:15 +0800

    feat: init

--- chroot/main.go
+++ chroot/main.go
@@ -1,29 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
 	"syscall"
 )
 
+// 注意,使用goland时，需要在 setting -> go -> build tag 切换 os 为 linux
 func main() {
 	oldRootF, err := os.Open("/")
 	defer oldRootF.Close()
 	if err != nil {
-		println("fail to open root: %v\n", err)
+		fmt.Printf("fail to open root: %v\n", err)
 	}
 
-	// change working dir to old root
-	err =
+	err = syscall.Chroot("/root/new-root")
 	if err != nil {
-		println("fail to chroot %v\n", err)
+		fmt.Printf("fail to chroot %v\n", err)
+	}
+
+	cmd := exec.Command("/bin/ls", "-l")
+	_, err = cmd.Output()
+	if err != nil {
+		fmt.Println("Failed to execute command:", err)
+		//return
 	}
 
 	err = oldRootF.Chdir()
 	if err != nil {
-		println("chdir() err: %v", err)
+		fmt.Printf("chdir() err: %v", err)
 	}
-	err = syscall.Chroot(".")
+	err = syscall.Chroot("/")
 	if err != nil {
-		println("chroot back err: %v", err)
+		fmt.Printf("chroot back err: %v", err)
 	}
+
+	cmd = exec.Command("/bin/ls", "-l")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Failed to execute command:", err)
+		return
+	}
+
+	fmt.Println(string(output))
 }
